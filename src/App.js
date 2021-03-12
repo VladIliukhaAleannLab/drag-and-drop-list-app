e17e886ddf124f735da952e5caeb9fc570a45c14
Alex Stupak <stupak.alexn@gmail.com>
2021-03-12 15:53:36 +0200

    create app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const AppContainer = styled.div`
+
+`;
+
+export default () => {
+  return (
+    <AppContainer className="App">
+      Created
+    </AppContainer>
+  );
+}
+
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+
+if (!Object.entries)
+   Object.entries = function( obj ){
+      let ownProps = Object.keys( obj ),
+         i = ownProps.length,
+         resArray = new Array(i); // preallocate the Array
+
+      while (i--)
+         resArray[i] = [ownProps[i], obj[ownProps[i]]];
+      return resArray;
+   };
+
+if(!Object.values) {
+  Object.values = function (obj) {
+    let vals = [];
+    for (const prop in obj) {
+        vals.push(obj[prop]);
+    }
+    return vals;
+  }
+}
+
+ReactDOM.render(<App {...root.dataset} />, root);
+
